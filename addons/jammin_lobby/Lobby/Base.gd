06f63753c8df22c9c4c5668b287a3bce6d0a47ac
Jamon Holmgren <code@jamon.dev>
2025-03-02 13:49:25 -0800

    Add project, refactoring WIP

--- addons/jammin_lobby/Lobby/Base.gd
+++ addons/jammin_lobby/Lobby/Base.gd
@@ -6,56 +6,6 @@ static var debug := OS.is_debug_build()
 func wait(time: float):
 	return Lobby.get_tree().create_timer(time).timeout
 
-# Network utilities
-func get_local_ipv4_addresses() -> Array[String]:
-	var ipv4_addresses: Array[String] = []
-	for address in IP.get_local_addresses():
-		if not is_good_address(address): continue
-		ipv4_addresses.append(address)
-	return ipv4_addresses
-
-func get_external_ip() -> Array:
-	var http = HTTPRequest.new()
-	add_child(http)
-	http.request("https://api.ipify.org")
-	var result = await http.request_completed
-	http.queue_free()
-	var wan_ip = result[3].get_string_from_utf8()
-	if not is_good_address(wan_ip): return [ERR_CANT_RESOLVE, ""]
-	return [OK, wan_ip]
-
-# Tries to get the router IP address (kinda slow)
-# Tested on:
-#   - ✅ macOS 15.1.1
-#   - ❓ Windows 10
-#   - ❓ Windows 11
-#   - ❓ Linux
-func get_router_ip():
-	var output := []
-	var exit_code: int
-	
-	var router_ip: String = ""
-
-	match OS.get_name():
-		"Windows":
-			exit_code = OS.execute("ipconfig", [], output)
-			for line in output[0].split("\n"): if "Default Gateway" in line: router_ip = line.split(":")[-1].strip_edges()
-		"macOS":
-			exit_code = OS.execute("netstat", ["-nr"], output)
-			for line in output[0].split("\n"): if "default" in line: router_ip = line.split()[1]
-		"Linux":
-			exit_code = OS.execute("ip", ["route", "show", "default"], output)
-			if output[0]: router_ip = output[0].split()[2]
-
-	if is_good_address(router_ip): return router_ip
-	return null
-
-func is_good_address(address: String) -> bool:
-	if not address.is_valid_ip_address(): return false
-	if address.begins_with("127."): return false
-	if address.split(".").size() != 4: return false
-	return true
-
 # Serialization utilities
 func to_dict(obj: Object, props: Array[String]) -> Dictionary:
 	var dict = {}
@@ -191,6 +141,4 @@ func every(frames: int, fn: Callable, offset: int = 0, debug_label: StringName =
 			fn.call_deferred()
 
 func exec_every(fn: Callable, debug_label: StringName = &""):
-	if debug_label != &"": Perf.start(debug_label)
 	fn.call()
-	if debug_label != &"": Perf.stop(debug_label)
---
+++ addons/jammin_lobby/Lobby/NetworkUtils.gd
@@ -0,0 +1,62 @@
+class_name NetworkUtils
+
+# Some network utilities
+#
+# NetworkUtils.get_local_ipv4_addresses()
+#   Returns an array of local IPv4 addresses
+#
+# NetworkUtils.get_external_ip()
+#   Returns the external IP address by pinging api.ipify.org
+#
+# NetworkUtils.is_good_address(address: String) -> bool
+#   Returns true if the address is a good address
+
+# Network utilities
+static func get_local_ipv4_addresses() -> Array[String]:
+	var ipv4_addresses: Array[String] = []
+	for address in IP.get_local_addresses():
+		if not is_good_address(address): continue
+		ipv4_addresses.append(address)
+	return ipv4_addresses
+
+static func get_external_ip() -> Array:
+	var http = HTTPRequest.new()
+	add_child(http)
+	http.request("https://api.ipify.org")
+	var result = await http.request_completed
+	http.queue_free()
+	var wan_ip = result[3].get_string_from_utf8()
+	if not is_good_address(wan_ip): return [ERR_CANT_RESOLVE, ""]
+	return [OK, wan_ip]
+
+# Tries to get the router IP address (kinda slow)
+# Tested on:
+#   - ✅ macOS 15.1.1
+#   - ❓ Windows 10
+#   - ❓ Windows 11
+#   - ❓ Linux
+static func get_router_ip():
+	var output := []
+	var exit_code: int
+	
+	var router_ip: String = ""
+
+	match OS.get_name():
+		"Windows":
+			exit_code = OS.execute("ipconfig", [], output)
+			for line in output[0].split("\n"): if "Default Gateway" in line: router_ip = line.split(":")[-1].strip_edges()
+		"macOS":
+			exit_code = OS.execute("netstat", ["-nr"], output)
+			for line in output[0].split("\n"): if "default" in line: router_ip = line.split()[1]
+		"Linux":
+			exit_code = OS.execute("ip", ["route", "show", "default"], output)
+			if output[0]: router_ip = output[0].split()[2]
+
+	if is_good_address(router_ip): return router_ip
+	return null
+
+static func is_good_address(address: String) -> bool:
+	if not address.is_valid_ip_address(): return false
+	if address.begins_with("127."): return false
+	if address.split(".").size() != 4: return false
+	return true
